9409473caf63214d640b15d2a071d8e343a1427e
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-18 08:13:42 +0100

    Add @Valid attribute and BindingResuld

--- src/main/java/com/example/demo/domain/Project.java
+++ src/main/java/com/example/demo/domain/Project.java
@@ -9,6 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -18,25 +22,33 @@ public class Project {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "Project name is required")
 	@Column(name = "PROJECT_NAME")
 	private String projectName;
 
-	@Column(name = "PROJECT_IDENTIFIER")
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+	@Column(name = "PROJECT_IDENTIFIER", updatable = false, unique = true)
 	private String projectIdentifier;
 
 	@Column(name = "DESCRIPTION")
+	@NotBlank(message = "Project description is required")
 	private String description;
 
 	@Column(name = "START_DATE")
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date startDate;
 
 	@Column(name = "END_DATE")
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date endDate;
 
 	@Column(name = "CREATED_AT")
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date createdAt;
 
 	@Column(name = "UPDATED_AT")
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updatedAt;
 
 	public Project() {
